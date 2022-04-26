d9ea1c5200ec44f6d513b3394c471e560885c6ab
anatoly <diggge@mail.ru>
2022-04-27 00:00:09 +0900

    100422d3jhcaaxsdfg1

--- lesson3/07_wall.py
+++ lesson3/07_wall.py
@@ -3,27 +3,25 @@
 # (цикл for)
 import simple_draw as sd
 sd.resolution = (800, 500)
-
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
-color = sd.random_color()
 # TODO здесь ваш код
-
-#     xx=xx+10
-#     yy=yy-5
-#     rainbow_start= sd.get_point(50+xx,50+yy)
-#     rainbow_end = sd.get_point(350+xx, 450+yy)
-
-# print(sd.resolution[0])
+yk=sd.resolution[1]//50
+yn=sd.resolution[0]//100
+print(yk)
+print(yn)
 y1=1
-for j in range(20):
-    x1=sd.random_number(-130,0)
-    x1=x1+30
-    for i in range(10):
-        sd.rectangle(sd.get_point(x1, y1), sd.get_point(x1 + 100, y1 + 50), color=sd.COLOR_ORANGE, width=2)
+for j in range(yk):
+    x1=sd.random_number(-20,0)
+    if j%2==0:
+        x1=x1+50
+    else:
+        x1=x1
+    for i in range(yn+1):
+        sd.rectangle(sd.get_point(x1, y1), sd.get_point(x1 + 100, y1 + 50), color=sd.COLOR_ORANGE, width=1)
         x1 = x1 + 100
         if x1 > sd.resolution[0]:
-            print(i)
+            # print(i)
             break
     y1 = y1 + 50
     if y1>sd.resolution[1]:
--- lesson3/08_smile.py
+++ lesson3/08_smile.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
-
+import simple_draw as sd
+sd.resolution=(800,500)
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
-
 # TODO здесь ваш код
-
-simple_draw.pause()
+for i in range(10):
+    x = sd.random_number(0, sd.resolution[0]-50)
+    y = sd.random_number(0, sd.resolution[1]-70)
+    color = sd.random_color()
+    sd.ellipse(sd.get_point(x, y), sd.get_point(x + 130, y + 100), color=color, width=1)
+    sd.circle(sd.get_point(x + 130 * 0.66, y + 100 * 0.65), radius=7, color=color, width=1)
+    sd.circle(sd.get_point(x + 130 * 0.33, y + 100 * 0.65), radius=7, color=color, width=1)
+    sd.lines([sd.get_point(x + 130 * 0.2, y + 100 * 0.4), sd.get_point(x + 130 * 0.3, y + 100 * 0.31),
+    sd.get_point(x + 130 * 0.4, y + 100 * 0.28), sd.get_point(x + 130 * 0.5, y + 100 * 0.26),sd.get_point(x + 78, y + 100 * 0.28), sd.get_point(x + 91, y + 31), sd.get_point(x + 104, y + 40)], color=color, closed=False, width=1)
+sd.pause()
