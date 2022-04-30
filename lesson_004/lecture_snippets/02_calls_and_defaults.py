0b7229c1342f1329af5058deeb355aa539d5aea0
anatoly <diggge@mail.ru>
2022-05-01 00:39:50 +0900

    100422d3jhcaaxsdfg1 1

--- lesson_004/lecture_snippets/02_calls_and_defaults.py
+++ lesson_004/lecture_snippets/02_calls_and_defaults.py
@@ -21,33 +21,23 @@ print(res)
 # можно совмещать
 res = vector_module(2, 3, z=4)
 print(res)
-
-
 # можно потребовать, что бы при вызове некоторые параметры указывались явно
 # это будут все параметры, которые идут после *
 def vector_module(x, y, *, z):
     return (x ** 2 + y ** 2 + z ** 2) ** .5
-
-
 # не пройдёт
 res = vector_module(2, 3, 4)
 # все нормально
 res = vector_module(2, 3, z=4)
 res = vector_module(z=4, x=2, y=3)
-
-
 # можно потребовать указание всех параметров явно
 def vector_module(*, x, y, z):
     return (x ** 2 + y ** 2 + z ** 2) ** .5
-
-
 # не пройдёт
 res = vector_module(2, 3, 4)
 res = vector_module(2, 3, z=4)
 # все нормально
 res = vector_module(z=4, x=2, y=3)
-
-
 # неправильные вызовы функций
 # res = vector_module()  # не передали ни одного параметра
 # res = vector_module(2, 3)  # передали мало параметров
--- lesson_004/probe.py
+++ lesson_004/probe.py
@@ -1,6 +1,31 @@
-if 2 > 1:
-    e, f = 5, 6
-    print('if:', e + f)
-# else:
-#     e, f = 7, 8
-print('global:', e + f)
+# решение проблемы
+def add_element_to_list(element, list_to_add=None):
+    """добавляем элемент к списку"""
+    if list_to_add is None:
+        list_to_add = []
+    list_to_add.append(element)
+    return list_to_add
+
+my_list = [3, 4, 5]
+add_element_to_list(element=1, list_to_add=my_list)
+print(my_list)
+new_list = add_element_to_list(element=1)
+print(new_list)
+# add_element_to_list(element=7, list_to_add=new_list)
+# print(new_list)
+
+# other_new_list = add_element_to_list(element=2)
+# print(other_new_list)
+# print(new_list)
+# решение проблемы
+# def add_element_to_list(element, list_to_add=None):
+#     """добавляем элемент к списку"""
+#     if list_to_add is None:
+#         list_to_add = []
+#     list_to_add.append(element)
+#     return list_to_add
+#
+#
+# my_list = [3, 4, 5]
+# add_element_to_list(element=1, list_to_add=my_list)
+# print(my_list)
