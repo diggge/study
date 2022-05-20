d4718998d316ea6f98e0881f9fd4f0b44400a8a9
anatoly <diggge@mail.ru>
2022-05-20 17:04:12 +0900

    началоfgh

--- work/zapros_zvonki.py
+++ work/zapros_zvonki.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import pymysql.cursors
 
+
 con = pymysql.connect(host='10.77.3.24',
                       user='ivanov-aa',
                       password='Ctynz,hm2',
@@ -11,13 +12,19 @@ con = pymysql.connect(host='10.77.3.24',
 with con:
     cur = con.cursor()
     cur.execute(
-        "SELECT callingstationid,calledstationid,acctstarttime,acctsessiontime FROM `history_phone` WHERE (acctstarttime>'2022-05-01 00:00:00' AND acctstoptime<'2022-07-01 10:00:00') ORDER BY acctstoptime")
+        "SELECT callingstationid,calledstationid,acctstarttime,acctsessiontime,nasipaddress FROM `history_phone` WHERE (acctstarttime>'2022-05-01 00:00:00' AND acctstoptime<'2022-07-01 10:00:00') ORDER BY acctstoptime")
     rows = cur.fetchall()
     for row in rows:
-        if row["calledstationid"] == '2356':
-            print('На указанный номер звонили с номера: ', row["callingstationid"], 'Время и Дата звонка:', row["acctstarttime"], 'Длительность звонка:' ,row["acctsessiontime"])
-        elif row["callingstationid"] == '2356':
-            print('Указанный номер звонил на номера: ', row["calledstationid"], 'Время и Дата звонка:', row["acctstarttime"], 'Длительность звонка:' ,row["acctsessiontime"])
+        if (row["callingstationid"] == '1096' or row["calledstationid"] == '1096'):
+            print('Кто звонил: ', row["callingstationid"], 'Кому звонил:', row["calledstationid"], 'Время и Дата звонка:', row["acctstarttime"], 'Длительность звонка:', row["acctsessiontime"])
+        # if row["calledstationid"] == '2510':
+        #     print('На указанный номер звонили с номера: ', row["callingstationid"], 'Время и Дата звонка:', row["acctstarttime"], 'Длительность звонка:' ,row["acctsessiontime"])
+        # elif row["callingstationid"] == '2510':
+
+        #     print('Указанный номер звонил на номера: ', row["calledstationid"], 'Время и Дата звонка:', row["acctstarttime"], 'Длительность звонка:' ,row["acctsessiontime"])
+        # # elif row["nasipaddress"] == '10.200.0.8':
+        #
+        #     pprint('Кто звонил: ', row["callingstationid"], 'Кому звонил',row["calledstationid"], 'Время и Дата звонка:', row["acctstarttime"], 'Длительность звонка:', row["acctsessiontime"])
         # else:
         #     print('Не нашли звонков')
         #      continue
