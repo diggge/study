00bbf458d0979f1c50959f65dace840f8358fe9e
anatoly <diggge@mail.ru>
2023-02-06 21:33:09 +0900

    1904227

--- lesson3new/00_bubbles.py
+++ lesson3new/00_bubbles.py
@@ -4,11 +4,25 @@ import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
+
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
 # TODO здесь ваш код
-
+# point=sd.get_point(300,200)
+# radius=50
+# for _ in range(100):
+#     radius+=5
+#     sd.circle(point,radius,width=2)
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
 # TODO здесь ваш код
+def bubble(point, step, kolichestvo_sharikov):
+    radius = 50
+    for _ in range(kolichestvo_sharikov):
+        radius += step
+        sd.circle(point, radius, width=3)
+
+
+point = sd.get_point(300, 200)
+bubble((100, 100), 3, 2)
 
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
@@ -20,5 +34,3 @@ sd.resolution = (1200, 600)
 # TODO здесь ваш код
 
 sd.pause()
-
-
--- lesson3new/probe.py
+++ lesson3new/probe.py
@@ -73,12 +73,88 @@
 #     total_mass += mass
 # print('Общая масса животных', total_mass)
 
-def some_func():
-    print('Привет! Я функция')
-
-
-# some_func()
+# def some_func():
+#     print('Привет! Я функция')
 #
-my_list = [3, 14, 15, 92, 6]
-for element in my_list:
-    some_func()
+#
+# # some_func()
+# #
+# my_list = [3, 14, 15, 92, 6]
+# for element in my_list:
+#     print(element)
+#     some_func()
+# def func_with_params(param):
+#     print('Функцию вызвали с параметром=', param)
+#
+#
+# my_list = [3, 14, 15, 92, 6]
+# for element in my_list:
+#     print('Начало цикла')
+#     func_with_params(param=element)
+#     print('Конец цикла')
+
+# # возврат значения из функции
+#
+# def power(number, pow):
+#     print('Функцию вызвали с параметрами', number, pow)
+#     power_value = number ** pow
+#     return power_value
+#
+#
+# my_list = [3, 14, 15, 92, 6]
+# for element in my_list:
+#     result = power(element, 10)
+#     print(result)
+#
+# for element in my_list:
+#     result = power(number=element, pow=element)
+#     print(result)
+# def some_func():
+#     print("я ничего не верну")
+#
+# result = some_func()
+# print(result)
+
+# можно возвращать несколько значений
+# def create_default_user():
+#     name = "Василий"
+#     age = 27
+#     return name, age
+# user_name, use_age=create_default_user()
+# print(user_name, use_age)
+# print(create_default_user())
+# динамическая типизация
+# def multiplay(number_1, number_2):
+#     print('Функцию вызвали с параметрами', number_1, number_2)
+#     value = number_1 * number_2
+#     return value
+# print(multiplay(number_1=42, number_2=27))
+# print(multiplay(number_1='привет! ', number_2=34))
+
+# Параметры передаются как ссылка
+
+def elephant_to_free(some_list):
+    elephant_found = 'elephant' in some_list
+    if elephant_found:
+        some_list.remove('elephant')
+        print('Слон на свободе!!!')
+    return elephant_found
+
+
+zoo = ['lion', 'elephant', 'monkey', 'skunk', 'horse', 'elephant']
+
+elephant_to_free(some_list=tuple(zoo))
+print(zoo)
+
+elephant_to_free(zoo)
+print(zoo)
+
+elephant_to_free(zoo)
+print(zoo)
+
+# это т.н. функции с побочными эффектами, они меняют контекст выполнения.
+# можно заблокировать изменение параметров - передать тьюпл
+zoo = ('lion', 'elephant', 'monkey', 'skunk', 'horse', 'elephant')
+elephant_to_free(zoo)
+print(zoo)
+
