713c48715b9cab0a7c96dd0059518e201ba0934e
anatoly <diggge@mail.ru>
2023-02-07 21:36:37 +0900

    1904227

--- lesson3new/00_bubbles.py
+++ lesson3new/00_bubbles.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 
 import simple_draw as sd
 
@@ -14,23 +15,31 @@ sd.resolution = (1200, 600)
 #     sd.circle(point,radius,width=2)
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
 # TODO здесь ваш код
-def bubble(point, step, kolichestvo_sharikov):
+def bubble(point, step, kolichestvo_sharikov,color):
     radius = 50
     for _ in range(kolichestvo_sharikov):
         radius += step
-        sd.circle(point, radius, width=3)
+        sd.circle(center_position=point, radius=radius,color=color,width=1)
 
 
-point = sd.get_point(300, 200)
-bubble((100, 100), 3, 2)
+# point = sd.get_point(300, 200)
+# bubble(point, 5, 20)
 
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
-
+# for x in range(100,1001,100):
+#     for y in range(100,301,100):
+#         point=sd.get_point(x,y)
+#         bubble(point,3,3)
 # Нарисовать три ряда по 10 пузырьков
 # TODO здесь ваш код
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 # TODO здесь ваш код
-
+for _ in range(100):
+    point=sd.random_point()
+    color=sd.random_color()
+    step=random.randint(3,10)
+    kolichestvo_sharikov=random.randint(1,15)
+    bubble(point,step,kolichestvo_sharikov,color=color )
 sd.pause()
