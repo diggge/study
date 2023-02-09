b040a9f49d64a64b9b176596551562ea112d0d0c
anatoly <diggge@mail.ru>
2023-02-09 22:20:13 +0900

    07022022 1cxv

--- lesson3new/test.py
+++ lesson3new/test.py
@@ -1,7 +1,23 @@
-user_input = input("Введите, пожалуйста, номер месяца: ")
-month = int(user_input)
-if type(month) == int or float:
-    print(type(month))
-    print('целое число')
-else:
-    print('не целое число')
+zoo_pet_mass = {
+    'lion': 300,
+    'skunk': 5,
+    'elephant': 5000,
+    'horse': 400,
+}
+total_mass = 0
+for animal in zoo_pet_mass:
+    print(animal, zoo_pet_mass[animal])
+    total_mass += zoo_pet_mass[animal]
+print('Общая масса животных', total_mass)
+
+total_mass = 0
+for animal, mass in zoo_pet_mass.items():
+    print(animal, mass)
+    total_mass += mass
+print('Общая масса животных', total_mass)
+
+total_mass = 0
+for mass in zoo_pet_mass.values():
+    print(mass)
+    total_mass += mass
+print('Общая масса животных', total_mass)
