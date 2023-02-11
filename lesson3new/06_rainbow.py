ab772bed2ad4a5dd3273e02fafea205553e46050
anatoly <diggge@mail.ru>
2023-02-11 11:24:31 +0900

    1904227

--- lesson3new/06_rainbow.py
+++ lesson3new/06_rainbow.py
@@ -14,13 +14,20 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 #         Нарисовать линию цветом color
 #         Начиная с точки start
 #         Заканчивая точкой end
-start_point=(50,50)
-end_point=(350,450)
-line(start_point,end_point,color=COLOR_YELLOW, width=4)
-
+# x0,y0,xf,yf=50,50,350,450
+# for color in rainbow_colors:
+#     x0+=5
+#     xf+=5
+#     sd.line(sd.get_point(x0,y0), sd.get_point(xf,yf), color=color, width=4)
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
 # TODO здесь ваш код
-
+# def circle(center_position, radius=50, color=COLOR_YELLOW, width=1):
+radius=400
+step=30
+for color in rainbow_colors:
+    center_position=sd.get_point(250,-100)
+    radius=radius+step
+    sd.circle(center_position,radius,color,width=30)
 sd.pause()
