3d19c6310cbba4cf9c58a5baba9bdb8b5cab0d96
anatoly <diggge@mail.ru>
2023-02-11 09:48:24 +0900

    07022022 1cxv

--- lesson3new/06_rainbow.py
+++ lesson3new/06_rainbow.py
@@ -9,6 +9,14 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 # TODO здесь ваш код
+# def line(start_point, end_point, color=COLOR_YELLOW, width=1):
+#     """
+#         Нарисовать линию цветом color
+#         Начиная с точки start
+#         Заканчивая точкой end
+start_point=(50,50)
+end_point=(350,450)
+line(start_point,end_point,color=COLOR_YELLOW, width=4)
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
