7750e541d0ee5a2aabce6509d788d0a626dc27d4
anatoly <diggge@mail.ru>
2023-02-11 12:19:39 +0900

    1904227

--- lesson3new/07_wall.py
+++ lesson3new/07_wall.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
-
 # (цикл for)
-import simple_draw
-
+import simple_draw as sd
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
-
 # TODO здесь ваш код
-
-simple_draw.pause()
+# def rectangle(left_bottom, right_top, color=COLOR_YELLOW, width=0):
+# sd.rectangle(sd.get_point(50,50),sd.get_point(600,600),sd.COLOR_RED,width=1)
+y0=-50
+quantity_x=sd.resolution[0]//100
+quantity_y=sd.resolution[1]//50
+print(quantity_x,quantity_y)
+for j in range(quantity_y):
+    y0 = y0 + 50
+    if j % 2== 0:
+        x0= 0
+    else:
+        x0=-50
+    for i in range(quantity_x):
+        sd.rectangle(sd.get_point(x0,y0),sd.get_point(x0+100,y0+50),sd.COLOR_ORANGE,width=1)
+        x0, y0 = x0 + 100, y0
+sd.pause()
