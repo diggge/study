0c67f48029cc11d855d3ad7915d067975eca0877
anatoly <diggge@mail.ru>
2023-02-15 20:26:11 +0900

    1904227

--- lesson3new/test.py
+++ lesson3new/test.py
@@ -1,23 +1,2 @@
-zoo_pet_mass = {
-    'lion': 300,
-    'skunk': 5,
-    'elephant': 5000,
-    'horse': 400,
-}
-total_mass = 0
-for animal in zoo_pet_mass:
-    print(animal, zoo_pet_mass[animal])
-    total_mass += zoo_pet_mass[animal]
-print('Общая масса животных', total_mass)
-
-total_mass = 0
-for animal, mass in zoo_pet_mass.items():
-    print(animal, mass)
-    total_mass += mass
-print('Общая масса животных', total_mass)
-
-total_mass = 0
-for mass in zoo_pet_mass.values():
-    print(mass)
-    total_mass += mass
-print('Общая масса животных', total_mass)
+zoo = ['lion', 'elephant', 'monkey', 'skunk', 'horse', 'elephant']
+print(dir(zoo))
--- lesson_004/probe.py
+++ lesson_004/probe.py
@@ -1,15 +1,3 @@
-def simple():
-    # Локальное пространство имен
-    print('simple:', a + b)
-    a = 9
-    print('simple:', a + b)
-
-
-# параметры - это локальные переменные
-def simple_3(a, b):
-    print('simple:', a + b)
-
-
-a, b = 2, 2
-print('global', a+b)
-simple_3(a=3, b=4)
+n=9
+for i in range(1,n,1):
+    print(i)
