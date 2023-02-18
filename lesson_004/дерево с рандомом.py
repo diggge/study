7a59848053932b66a39e45193f1c4cd680ed2839
anatoly <diggge@mail.ru>
2023-02-18 17:21:05 +0900

    07022022 1cxv

--- lesson_004/дерево с рандомом.py
+++ lesson_004/дерево с рандомом.py
@@ -1,5 +1,5 @@
 import simple_draw as sd
-sd.resolution = (1500, 1000)
+sd.resolution = (1000, 900)
 def draw_branches(point0,angle0,angle,length,width):
     # print(point0,angle0,angle,length,width)
     v1 = sd.get_vector(start_point=point0,angle=angle0+angle,length=length,width=width)
@@ -10,9 +10,9 @@ def draw_branches(point0,angle0,angle,length,width):
         v1.draw()
         v2.draw()
     while length>10:
-         x=sd.random_number(70,110)
+         x=sd.random_number(70,100)
          y=sd.random_number(90,110)
-         length=length*0.75*x/100
+         length=length*0.8*x/100
          angle=angle*y/100
          width=width-4
          print(x,y)
@@ -24,7 +24,7 @@ def draw_branches(point0,angle0,angle,length,width):
              draw_branches(point0=v2.end_point, angle0=angle0-angle,angle=angle, length=length,width=1)
     else:
         return
-v = sd.get_vector(start_point=sd.get_point(750,30), angle=90, length=100, width=10)
+v = sd.get_vector(start_point=sd.get_point(500,30), angle=90, length=100, width=10)
 v.draw()
 point0=v.end_point
 draw_branches(point0=point0,angle0=90,angle=30,length=150,width=7)
