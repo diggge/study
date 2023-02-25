673bd09e1f727490c558bd7778c03c66e6c7848f
anatoly <diggge@mail.ru>
2023-02-25 19:13:41 +0900

    1904227

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -4,5 +4,8 @@
 # Формат: В комнате room_1 живут: ...
 
 # TODO здесь ваш код
+from room_1 import folks as folks_room1
+from room_2 import folks as folks_room2
 
-
+print('В комнате 1 живут:', folks_room1)
+print('В комнтае 2 живут:', folks_room2)
--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -6,6 +6,18 @@
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
 # TODO здесь ваш код
+from district.central_street.house1.room1 import folks as folks_CS_h1_r1
+from district.central_street.house1.room2 import folks as folks_CS_h1_r2
+from district.central_street.house2.room1 import folks as folks_CS_h2_r1
+from district.central_street.house2.room2 import folks as folks_CS_h2_r2
+from district.soviet_street.house1.room1 import folks as folks_SS_h1_r1
+from district.soviet_street.house1.room1 import folks as folks_SS_h1_r2
+from district.soviet_street.house2.room1 import folks as folks_SS_h2_r1
+from district.soviet_street.house2.room1 import folks as folks_SS_h2_r2
+folks=folks_CS_h1_r1+folks_CS_h1_r2+folks_CS_h2_r1+folks_CS_h2_r2+folks_SS_h1_r1+folks_SS_h1_r2+folks_SS_h2_r1+folks_SS_h2_r2
+print('На районе живут (1 вариант)',folks)
+folks_str=",".join(folks)
+print('На районе живут (2 вариант)',folks_str)
 
 
 
