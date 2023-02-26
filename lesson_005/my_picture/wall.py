0b38fbd8716ba4267c1b543ebfa03d9ea02201b9
anatoly <diggge@mail.ru>
2023-02-26 18:01:30 +0900

    07022022 1cxv

--- lesson_005/my_picture/wall.py
+++ lesson_005/my_picture/wall.py
@@ -1,16 +1,9 @@
 import simple_draw as sd
-def paint_wall(quantity_x,quantity_y,color,width):
+def paint_wall(x0,quantity_x,quantity_y,color,width):
     y0=-50
     for j in range(quantity_y):
         y0 = y0 + 50
-        if j % 2== 0:
-            x0= 0
-        else:
-            x0=-50
-        for i in range(quantity_x):
-            sd.rectangle(sd.get_point(x0,y0),sd.get_point(x0+100,y0+50),color=color,width=width)
-            x0, y0 = x0 + 100, y0
-quantity_x=sd.resolution[0]//100
-quantity_y=sd.resolution[1]//50
-paint_wall(quantity_x=sd.resolution[0]//100,quantity_y=sd.resolution[1]//50,color=sd.COLOR_ORANGE,width=1)
+        sd.line(sd.get_point(x0,y0),sd.get_point(x0+quantity_x,y0),color=color,width=width)
+        # sd.rectangle(sd.get_point(x0, y0), sd.get_point(sd.resolution[0], y0 + 50), color=color, width=width)
+paint_wall(x0=50,quantity_x=sd.resolution[0]-100,quantity_y=sd.resolution[1]//50,color=sd.COLOR_DARK_ORANGE,width=2)
 sd.pause()
--- lesson_005/my_picture/windows.py
+++ lesson_005/my_picture/windows.py
@@ -1,8 +1,17 @@
 import simple_draw as sd
-sd.resolution = (1000, 900)
 print(sd.resolution[0])
 def paint_windows(center_position,length_x,length_y):
     sd.rectangle(sd.get_point(center_position[0]-length_x/2,center_position[1]-length_y/2),sd.get_point(center_position[0],center_position[1]+length_y/2),color=sd.COLOR_WHITE,width=1)
     sd.rectangle(sd.get_point(center_position[0],center_position[1]-length_y/2),sd.get_point(center_position[0]+length_x/2,center_position[1]+length_y/2),color=sd.COLOR_WHITE,width=1)
+    v_ln = sd.get_vector(sd.get_point(center_position[0] - length_x / 2, center_position[1] - length_y / 2), 200, length=50, width=1)
+    v_lv = sd.get_vector(sd.get_point(center_position[0] - length_x / 2, center_position[1] + length_y / 2), 160, length=50, width=1)
+    v_pn = sd.get_vector(sd.get_point(center_position[0] + length_x / 2, center_position[1] - length_y / 2), 340, length=50, width=1)
+    v_vv = sd.get_vector(sd.get_point(center_position[0] + length_x / 2, center_position[1] + length_y / 2), 20, length=50, width=1)
+    v_ln.draw(color=sd.COLOR_WHITE)
+    v_lv.draw(color=sd.COLOR_WHITE)
+    v_pn.draw(color=sd.COLOR_WHITE)
+    v_vv.draw(color=sd.COLOR_WHITE)
+    sd.line(v_ln.end_point,v_lv.end_point,color=sd.COLOR_WHITE,width=1)
+    sd.line(v_pn.end_point, v_vv.end_point, color=sd.COLOR_WHITE, width=1)
 paint_windows((300,300),100,100)
 sd.pause()
