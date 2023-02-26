1540df1876f9acbb6dd8cb7bd86c71f4a1028b3b
anatoly <diggge@mail.ru>
2023-02-26 22:48:07 +0900

    07022022 1cxv

--- lesson_005/my_picture/house.py
+++ lesson_005/my_picture/house.py
@@ -1,5 +1,6 @@
-from house.wall import paint_wall as pw
+from house.wall import paint_wall
 import simple_draw as sd
-pw(x0=300,y0=50,quantity_x=300,quantity_y=5,color=sd.COLOR_DARK_ORANGE,width=2)
-sd.pause()
+sd.start_drawing()
+paint_wall(x0=100, y0=50, dlina_doma=300, shirina_brevna=25, kolichestvo_breven=8, color=sd.COLOR_DARK_ORANGE, width=3)
+sd.finish_drawing()
 
--- lesson_005/my_picture/house/wall.py
+++ lesson_005/my_picture/house/wall.py
@@ -1,10 +1,9 @@
 import simple_draw as sd
-def paint_wall(x0,y0,quantity_x,quantity_y,color,width):
-    for j in range(quantity_y+2):
-        y0 = y0 + 50
-        sd.line(sd.get_point(x0,y0),sd.get_point(x0+quantity_x,y0),color=color,width=width)
-        sd.circle(sd.get_point(x0,y0+25),25,color=color,width=width)
-        sd.circle(sd.get_point(x0+quantity_x, y0 + 25), 25, color=color, width=width)
-        print(quantity_x,quantity_y)
-paint_wall(x0=50,y0=50,quantity_x=300,quantity_y=5,color=sd.COLOR_DARK_ORANGE,width=2)
+def paint_wall(x0, y0, dlina_doma, kolichestvo_breven, shirina_brevna, color, width):
+    for j in range(kolichestvo_breven):
+        y0 = y0 + shirina_brevna
+        sd.line(sd.get_point(x0,y0), sd.get_point(x0 + dlina_doma, y0), color=color, width=width)
+        sd.circle(sd.get_point(x0,y0+shirina_brevna/2),shirina_brevna/2,color=color,width=width)
+        sd.circle(sd.get_point(x0 + dlina_doma, y0 + shirina_brevna/2), shirina_brevna/2, color=color, width=width)
+paint_wall(x0=100,y0=50,dlina_doma=300,shirina_brevna=25,kolichestvo_breven=9,color=sd.COLOR_DARK_ORANGE,width=2,)
 sd.pause()
