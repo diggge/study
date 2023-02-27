74d5bc45676f4370df949a845c0a5ad7a3e3a359
IvanovAA1604 <diggge@mail,ru>
2023-02-27 10:34:25 +0900

    123

--- lesson_005/my_picture/house.py
+++ lesson_005/my_picture/house.py
@@ -10,12 +10,12 @@ paint_windows((750,230),100,100)
 triangle(sd.get_point(600,340),300/(2*sd.cos(45)),45)
 #Лес
 for _ in range(5):
-    x0 = sd.random_number(900, 1800)
+    x0 = sd.random_number(1000, 1700)
     y0 = sd.random_number(0, 400)
-    v = sd.get_vector(start_point=sd.get_point(x0, y0), angle=90, length=80, width=7)
+    v = sd.get_vector(start_point=sd.get_point(x0, y0), angle=90, length=90, width=7)
     v.draw(color=sd.COLOR_DARK_ORANGE)
     point0 = v.end_point
-    draw_branches(point0=point0, angle0=90, angle=30, length=60, width=6,color=sd.COLOR_DARK_ORANGE)
+    draw_branches(point0=point0, angle0=90, angle=30, length=70, width=6,color=sd.COLOR_DARK_ORANGE)
 #Радуга
 raduga(sd.get_point(600,-200),1400,30)
 sd.pause()
