0843fec7420a5022c5b5f7416591ac560121bd3b
anatoly <diggge@mail.ru>
2023-03-17 21:59:21 +0900

    07022022 1cxv

--- lesson_006/game_mastermind/mastermind_game.py
+++ lesson_006/game_mastermind/mastermind_game.py
@@ -17,7 +17,16 @@ while True:
     print(x)
     kolichestvo_khodov=0
     while True:
-        y = input('Напишите вариант четырехзначного числа c неповторяющимися цифрами :   ')
+        while True:
+            try:
+                y = int(input('Напишите вариант четырехзначного числа c неповторяющимися цифрами :  '))
+                if y<10000 or y>1023:
+                    raise Exception
+                break
+            except ValueError:
+                print('Ошибка ввода!!!')
+            except Exception:
+                print('Напишите вариант четырехзначного числа c неповторяющимися цифрами еще раз:')
         chek_number(int(x), int(y))
         kolichestvo_khodov += 1
         if is_gameover():
