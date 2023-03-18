a26afb9442c178bbad3da443edab6710285f7b48
anatoly <diggge@mail.ru>
2023-03-18 19:22:20 +0900

    07022022 1cxv

--- lesson_006/game_mastermind/probe.py
+++ lesson_006/game_mastermind/probe.py
@@ -1,20 +1,31 @@
-list1 = [23, 56, 78, 69]
-list2 = [11, 89, 33, 45]
-# список для результата
-result = []
+while True:
+    try:
+        y = int(input('Напишите вариант четырехзначного числа c неповторяющимися цифрами :  '))
+        if (1023< y < 10000 and y // 1000!= y // 100 % 10|| y // 10 % 10, y % 10):
+            break
+        else:
+            raise ValueError()
+    except ValueError:
+        print("Ошибка! введенное число не отвечает требованиям")
 
-# перебираем элементы списка
-# в пределах длины первого списка
-for i in range(len(list1)):
-    # сравниваем элементы первого списка
-    # с элементами второго списка
-    if list1[i] < list2[i]:
-        # добавляем в итоговый список `result`
-        # элемент из первого, если он меньше
-        result.append(list1[i])
-    else:
-        # иначе - добавляем элемент из второго списка
-        result.append(list2[i])
 
-# выводим итоговый список
-print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# y // 1000, y // 100 % 10, y // 10 % 10, y % 10
+# while True:
+#     try:
+#         x = int(input("Please enter a number: "))
+#         break
+#     except ValueError:
+#         print("Oops!  That was no valid number.  Try again...")
