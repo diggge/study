708c17614620da413addad6c15d70249a8533723
anatoly <diggge@mail.ru>
2023-03-27 22:22:43 +0900

    падение одной снежинки через классы

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
-
+sd.resolution = (1200, 630)
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
 #  - отработку изменений координат
@@ -9,19 +9,42 @@ import simple_draw as sd
 
 
 class Snowflake:
-    pass
+    def __init__(self, number):
+        self.number = number
+        self.x = sd.random_number(0, 1100)
+        self.y = sd.random_number(600, 650)
+        self.length=sd.random_number(10, 15)
+        self.factor_a=(sd.random_number(1, 9)) / 10
+    def __str__(self):
+        return 'Стежинка № - {}, координаты x,y {},{}, размер снежинки {}, фактор_а {}'.format(self.number, self.x, self.y,self.length,self.factor_a)
+    def draw(self):
+        sd.start_drawing()
+        sd.snowflake(center=sd.get_point(self.x,self.y),length=self.length,color=sd.COLOR_WHITE,factor_a=self.factor_a)
+        sd.finish_drawing()
+        print('Рисую  снежинку № {} c параметрами x={},y={},length={},factor_a={}'.format(self.number,self.x, self.y,self.length,self.factor_a))
+    def move(self):
+        self.x += sd.random_number(-5,5)+1
+        self.y -= sd.random_number(0, 15)
+        print('Cместил снежинку № {} координату x={},координату y={}'.format(self.number, self.x, self.y))
+    def clear_previous_picture(self):
+        sd.clear_screen()
+    def can_fall(self):
+        if self.y<10:
+            self.x = sd.random_number(0, 1100)
+            self.y=sd.random_number(600, 650)
+            self.length = sd.random_number(10, 15)
+            self.factor_a = (sd.random_number(1, 9)) / 10
+# TODO здесь ваш код
+flake = Snowflake(1)
 
-    # TODO здесь ваш код
-
-
-flake = Snowflake()
 
 while True:
     flake.clear_previous_picture()
-    flake.move()
     flake.draw()
-    if not flake.can_fall():
-        break
+    flake.move()
+    flake.can_fall()
+    # if not flake.can_fall():
+    #     break
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
