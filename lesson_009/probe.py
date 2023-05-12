5112f8ac81fa92c07d0ff3ad25c399b78f5f1cb2
anatoly <diggge@mail.ru>
2023-05-12 11:52:40 +0900

    2 задача, log_parser FULL сделано

--- lesson_009/probe.py
+++ lesson_009/probe.py
@@ -1,4 +1,17 @@
-# форматирование вывода строк - минимальная ширина поля {xxx:10}
-phones = {'Bill': 4127, 'Jameson': 4098, 'Abraham': 7678}
-for name, phone in phones.items():
-    print('{name:10} ==> {phone:10d}'.format(name=name, phone=phone))
+import os
+
+# Все файлы лежат на диске и имеют путь в файловой системе. Как работать с файлами на уровне ОС?
+# Есть встроенные модули для этого: os, os.path, shutil
+# Пригодятся они для написания скриптов-аналогов bash (.bat файлов в Windows)
+
+path = 'C:/Windows/Help'
+path_normalized=os.path.normpath(path)
+# Пройтись по всем файлам в директории.
+for dirpath, dirnames, filenames in os.walk(path_normalized):
+    # print(dirpath, dirnames, filenames)
+    for file in filenames:
+        print(dirpath+'\\'+file,round(os.path.getsize(dirpath+'\\'+file)/1024,2),'КБ',os.path.getmtime(dirpath+'\\'+file))
+
+
+# os.path.normpath(path)
+
