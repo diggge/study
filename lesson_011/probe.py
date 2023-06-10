3984526ae4043e1cb8401e7940ce1936e107a9fd
anatoly <diggge@mail.ru>
2023-06-10 19:24:18 +0900

    lesson 10 решил 3ю задачу полностью

--- lesson_011/probe.py
+++ lesson_011/probe.py
@@ -1,9 +1,13 @@
-def fibonacci_v2(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
-fib = fibonacci_v2(n=10)
-print(fib)
-for value in fib:
-    print(value)
+# n = int(input("Введите число:"))
+n=3291566
+i=len(str(n))
+print("Количество цифр равно:", i)
+j=i//2
+summa_left = 0
+summa_right = 0
+for k in range(1,j+1):
+    summa_left += (n % 10**i) // 10**(i-1)
+    summa_right += (n % 10**(j)) // 10**(j-1)
+    i -= 1
+    j -= 1
+print(n,j,summa_left,summa_right)
