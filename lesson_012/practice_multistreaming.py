36524e3b02ca13599a8006266bc07648c0f5fa87
anatoly <diggge@mail.ru>
2023-10-19 06:29:45 +0900

    практика многопроцессорная делаю 09:07

--- lesson_012/practice_multistreaming.py
+++ lesson_012/practice_multistreaming.py
@@ -1,4 +1,4 @@
-import multiprocessing
+import threading
 
 import requests
 from threading import Thread
@@ -14,13 +14,12 @@ sites = [
     'https://soccer.ru',
     'https://sports.ru'
 ]
-class PagerSizer(multiprocessing.Process):
-    def __init__(self, url, collector, go_ahead=True,*args,**kwargs):
+class PagerSizer(threading.Thread):
+    def __init__(self, url, go_ahead=True,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.url = url
         self.total_bytes = 0
         self.go_ahead = go_ahead
-        self.collector = collector
     def run(self):
         self.total_bytes = 0
         html_data = self.get_html(url=self.url)
@@ -30,18 +29,13 @@ class PagerSizer(multiprocessing.Process):
         if self.go_ahead:
             extractor = LinkExtractor(base_url=self.url)
             extractor.feed(html_data)
-            collector = multiprocessing.Queue()
-            sizers = [PagerSizer(url=link,go_ahead=False, collector=collector) for link in extractor.links]
+            sizers = [PagerSizer(url=link,go_ahead=False) for link in extractor.links]
             for sizer in sizers:
                 sizer.start()
             for sizer in sizers:
                 sizer.join()
-            while not collector.empty():
-                data = collector.get()
-                self.total_bytes += data['total_bytes']
             for sizer in sizers:
                 self.total_bytes += sizer.total_bytes
-        self.collector.put(dict(url=self.url, total_bytes=self.total_bytes))
 
 
     def get_html(self,url):
@@ -54,17 +48,11 @@ class PagerSizer(multiprocessing.Process):
             return res.text
 @time_track
 def main():
-    collector = multiprocessing.Queue()
-    sizers = [PagerSizer(url=url,collector=collector) for url in sites]
+    sizers = [PagerSizer(url=url) for url in sites]
     for sizer in sizers:
         sizer.start()
     for sizer in sizers:
         sizer.join()
-
-    while not collector.empty():
-        data = collector.get()
-        print(f"For url {data['url']} need download {data['total_bytes']//1024} Kb ")
-
     for sizer in sizers:
         print(f'For url {sizer.url} need download {round(sizer.total_bytes/1024,2)} Kbytes')
 
